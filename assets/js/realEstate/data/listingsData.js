6acd619966814bab9088af6706bc5697a56264e4
Ian Baylis <ibay0918@gmail.com>
2017-12-19 00:36:03 -0500

    First App Commit

---
+++ assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,102 @@
+const listingsData = [
+  {
+    address: '2034 grand ave',
+    city: 'Philadelphia',
+    state: 'PA',
+    rooms: 3,
+    price: 220000,
+    floorSpace: 2000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    address: '2034 grand ave',
+    city: 'New York',
+    state: 'NY',
+    rooms: 2,
+    price: 240000,
+    floorSpace: 2000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Condo',
+    image: ''
+  },
+  {
+    address: '2034 UP ave',
+    city: 'Cherry Hill',
+    state: 'NJ',
+    rooms: 7,
+    price: 820000,
+    floorSpace: 4000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Single Home',
+    image: ''
+  },
+  {
+    address: '2034 left ave',
+    city: 'Philadelphia',
+    state: 'PA',
+    rooms: 0,
+    price: 120000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Studio',
+    image: ''
+  },
+  {
+    address: '2034 19th ave',
+    city: 'Philadelphia',
+    state: 'PA',
+    rooms: 6,
+    price: 278900,
+    floorSpace: 1000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Multi Home',
+    image: ''
+  },
+  {
+    address: '2034 north ave',
+    city: 'San Fransisco',
+    state: 'CA',
+    rooms: 9,
+    price: 220546,
+    floorSpace: 2000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Ranch',
+    image: ''
+  },
+  {
+    address: '2034 grand ave',
+    city: 'Orlando',
+    state: 'FL',
+    rooms: 1,
+    price: 1700000,
+    floorSpace: 2000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Room',
+    image: ''
+  }
+]
+
+export default listingsData;
